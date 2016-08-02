0355ff237a4007494adbd4d09c0f400bcfc04b20
Tomas Janousek <tomi@nomi.cz>
2016-08-02 09:17:07 +0200

    Fix maps when switching between analysis/overview

--- inject.js
+++ inject.js
@@ -20,7 +20,7 @@ var Arrive=function(a,b,c){"use strict";function l(a,b,c){e.addMethod(b,c,a.unbi
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-document.arrive("#map-type-control", {onceOnly: true, existing: true}, function(){
+document.arrive("#map-type-control", {onceOnly: false, existing: true}, function(){
 	var s = document.createElement("script");
 	s.src = chrome.extension.getURL('fix.js');
 	s.type = 'text/javascript';
