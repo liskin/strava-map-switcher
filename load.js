ba44c3d84a51cdac20e9d66c4af342021bc4ff9a
Tomas Janousek <tomi@nomi.cz>
2020-05-22 20:28:22 +0200

    Simplify loader a bit

--- load.js
+++ load.js
@@ -14,23 +14,17 @@
 	const baseUrl = document.currentScript.src.match("^[a-z-]+://.*/") + "";
 	const getURL = (path) => baseUrl + path;
 
-	const ignoreError = function (promise) {
-		return new Promise(function (resolve, reject) {
-			promise.finally(resolve);
-		});
-	};
+	const ignoreError = (promise) => new Promise(resolve => { promise.finally(resolve); null; });
 
-	const getScript = function (url) {
-		return new Promise(function (resolve, reject) {
-			const s = document.createElement("script");
-			s.src = url;
-			s.async = true;
-			s.type = 'text/javascript';
-			s.onerror = reject;
-			s.onload = resolve;
-			document.body.appendChild(s);
-		});
-	};
+	const getScript = (url) => new Promise(function (resolve, reject) {
+		const s = document.createElement("script");
+		s.src = url;
+		s.async = true;
+		s.type = 'text/javascript';
+		s.onerror = reject;
+		s.onload = resolve;
+		document.body.appendChild(s);
+	});
 
 	Promise.all([
 		getScript(getURL('arrive.min.js')),
