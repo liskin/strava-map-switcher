liskin/strava-map-switcher

commit e4b24948125ba98be87365eef08b98b73cfd51d3

layers: Use https proxy for mtbmap

layers.js
@@ -49,7 +49,8 @@ var AdditionalMapLayers;
 				{url: "https://mapserver.mapy.cz/hybrid-trail_bike-m/{z}-{x}-{y}",
 					opts: {minZoom: 2, maxZoom: 20, maxNativeZoom: 18, attribution: mapyCzAttr}}},
 		mtbmap: {name: "mtbmap.cz [Europe]",
-			url: "http://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png",
+			url: "https://nomi.cz/mtbmapcz-https-proxy/mtbmap_tiles/{z}/{x}/{y}.png",
+			// url: "http://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png",
 			opts: {minZoom: 3, maxZoom: 20, maxNativeZoom: 18, attribution: mtbMapAttr}},
 		freemapsk: {name: "freemap.sk [Europe south-east]",
 			url: "https://outdoor.tiles.freemap.sk/{z}/{x}/{y}",

commit 1ded148fb1020626ac266e8266d3758ec3a1914b

layers: Drop mtbmap proxy, no longer needed
mtbmap.cz has https now: https://twitter.com/mattes_CZ/status/1432722909940813829

Fixes: e4b24948125b ("layers: Use https proxy for mtbmap")

layers.js
@@ -15,7 +15,7 @@ var AdditionalMapLayers;
 {
 	const osmAttr = '&copy; <a href="https://openstreetmap.org" target="_blank">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>';
 	const thunderforestAttr = osmAttr + ', Tiles courtesy of <a href="https://www.thunderforest.com/" target="_blank">Andy Allan</a>';
-	const mtbMapAttr = osmAttr + ', Tiles courtesy of <a href="http://mtbmap.cz/" target="_blank">mtbmap.cz</a>';
+	const mtbMapAttr = osmAttr + ', Tiles courtesy of <a href="https://mtbmap.cz/" target="_blank">mtbmap.cz</a>';
 	const freeMapSkAttr = osmAttr + ', Tiles courtesy of <a href="https://freemap.sk/" target="_blank">freemap.sk</a>';
 	const mapyCzAttr = '&copy; <a href="https://www.seznam.cz/" target="_blank">Seznam.cz, a.s</a>, ' + osmAttr;
 	const cuzkAttr = '&copy; <a href="https://geoportal.cuzk.cz" target="_blank">ČÚZK</a>';
@@ -49,8 +49,7 @@ var AdditionalMapLayers;
 				{url: "https://mapserver.mapy.cz/hybrid-trail_bike-m/{z}-{x}-{y}",
 					opts: {minZoom: 2, maxZoom: 20, maxNativeZoom: 18, attribution: mapyCzAttr}}},
 		mtbmap: {name: "mtbmap.cz [Europe]",
-			url: "https://nomi.cz/mtbmapcz-https-proxy/mtbmap_tiles/{z}/{x}/{y}.png",
-			// url: "http://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png",
+			url: "https://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png",
 			opts: {minZoom: 3, maxZoom: 20, maxNativeZoom: 18, attribution: mtbMapAttr}},
 		freemapsk: {name: "freemap.sk [Europe south-east]",
 			url: "https://outdoor.tiles.freemap.sk/{z}/{x}/{y}",